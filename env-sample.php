f31428258ee20b1be4269be8c2f30be075358281
Nipuna Dodantenna <nipunadodan@gmail.com>
2019-06-14 17:14:15 +0530

    env sample changed

--- env-sample.php
+++ env-sample.php
@@ -8,4 +8,6 @@ define("DB_USER", "nipuna");
 define("DB_PASSWORD", "nipuna123");
 define("DB_PREFIX", "o_");
 
+define('APP_ID', ''); //set your unique app id here
+
 date_default_timezone_set('Asia/Colombo'); //set your timezone here
