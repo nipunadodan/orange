54f566679f7708c717f97868b9d0770c8fbbc956
Nipuna Dodantenna <nipunadodan@gmail.com>
2019-06-17 13:47:55 +0530

    added home page content

---
+++ pages/home.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="row min-vh-100 align-content-center text-center">
+        <div class="col-12">
+            <h1 class="text-thin text-loose">Orange</h1>
+            <hr>
+            <h2 class="h6 text-muted text-thin">Lightweight PHP Framework</h2>
+        </div>
+    </div>
+</div>
