32cfcb30ac698c0ce958079b7df7e549f7461cdc
Nipuna Dodantenna <nipunadodan@gmail.com>
2021-02-05 14:34:36 +0530

    openweather appid fix

--- processes/weather-process.php
+++ processes/weather-process.php
@@ -3,7 +3,7 @@ use Orange\ApiConnect;
 
 $data = [
     'q' => 'Colombo,LK',
-    'appid' => '0fda066fba8d3db963c55e3070fec64a',
+    'appid' => OPEN_WEATHER,
 ];
 $api = ApiConnect::getJSON('https://api.openweathermap.org/data/2.5/weather',$data);
 
